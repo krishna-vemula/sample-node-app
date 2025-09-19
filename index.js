59db1f53e633490b8e38c97a5fa00caf2818292f
krishna-vemula <85216403+krishna-vemula@users.noreply.github.com>
2025-09-19 12:37:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Jenkins!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+});
